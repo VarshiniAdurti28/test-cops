403098af2895a90487e636ad4f064ecb0ac8743d
Sahil kumar <143071869+Sahiiil1406@users.noreply.github.com>
2025-01-19 14:08:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
      res.send('Hello World ...Updated');
 })
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
      console.log(`Server is running on port ${PORT}`);
 })
